558bd462d478d4af3fa0292fe88bbd63a2a1105f
Tauchner <7229@htl.rennweg.at>
2021-05-11 15:09:26 +0200

    User Story 2

--- static.php
+++ static.php
@@ -2,14 +2,104 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.6/build/pure-min.css"
+          integrity="sha384-Uu6IeWbM+gzNVXJcM9XV3SohHtmWE+3VGi496jvgX1jyvDTXfdK+rfZc8C1Aehk5" crossorigin="anonymous">
     <title>USRPSSS</title>
 </head>
 <body>
 <header>
-
+    <h1>USRPS</h1>
 </header>
+
+<div>
+    <p>The USRPS is amazing and stuff, whatever who cares, here's your data</p>
+
+    <h2>Imaginatively amazing tournament</h2>
+    <h3>Game 1:</h3>
+    <p>On 2021-11-25T16:41:15+01:00</p>
+    <table class="pure-table pure-table-bordered">
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Symbol</th>
+        </tr>
+        <tr>
+            <td colspan="2">Game 1: 2021-02-14T12:23:51+01:00</td>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Champ</td>
+            <td>Rock</td>
+        </tr>
+        <tr>
+            <td>Hannes</td>
+            <td>Paper</td>
+        </tr>
+        </tbody>
+        <thead>
+        <tr>
+            <td colspan="2">Game 2: 2021-02-14T12:23:51+01:00</td>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Champ</td>
+            <td>Rock</td>
+        </tr>
+        <tr>
+            <td>Hannes</td>
+            <td>Paper</td>
+        </tr>
+        </tbody>
+        <thead>
+        <tr>
+            <td colspan="2">Game 3: 2021-02-14T12:23:51+01:00</td>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Champ</td>
+            <td>Rock</td>
+        </tr>
+        <tr>
+            <td>Hannes</td>
+            <td>Paper</td>
+        </tr>
+        </tbody>
+        <thead>
+        <tr>
+            <td colspan="2">Game 4: 2021-02-14T12:23:51+01:00</td>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Champ</td>
+            <td>Rock</td>
+        </tr>
+        <tr>
+            <td>Hannes</td>
+            <td>Paper</td>
+        </tr>
+        </tbody>
+        <thead>
+        <tr>
+            <td colspan="2">Game 5: 2021-02-14T12:23:51+01:00</td>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Champ</td>
+            <td>Rock</td>
+        </tr>
+        <tr>
+            <td>Hannes</td>
+            <td>Paper</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
 </body>
 </html>
